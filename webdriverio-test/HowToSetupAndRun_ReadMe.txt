677c42f4238d2152b91635292c7836723d1eff3f
JBaba <naimish101.viradia@gmail.com>
2016-11-13 16:27:44 -0500

    init somke test

---
+++ webdriverio-test/HowToSetupAndRun_ReadMe.txt
@@ -0,0 +1,32 @@
+All selenium version configuration
+----------------------------------
+J2SE 8 = 52,
+J2SE 7 = 51,
+J2SE 6.0 = 50,
+J2SE 5.0 = 49,
+JDK 1.4 = 48,
+JDK 1.3 = 47,
+JDK 1.2 = 46,
+JDK 1.1 = 45
+
+Java 8
+------
+$ curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
+
+Java 7
+------
+$ curl -O http://selenium-release.storage.googleapis.com/2.51/selenium-server-standalone-2.51.0.jar
+
+How to run run server
+---------------------
+$ java -jar -Dwebdriver.gecko.driver=./geckodriver selenium-server-standalone-x.x.x.jar
+
+
+Run Command
+-----------
+$ node test.js
+
+
+Run throught wdio command 
+---------------------------
+C:\DEV\Impact\WebDriverSmokeTest\webdriverio-test>"C:\Program Files (x86)\Java\jre1.8.0_111\bin\java" -jar -Dwebdriver.gecko.driver="C:\DEV\Impact\WebDriverSmokeTest\driver\geckodriver.exe"  selenium-server-standalone-3.0.1.jar
---
+++ webdriverio-test/test/specs/spec.js
@@ -0,0 +1,96 @@
+var assert = require('assert');
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.Should();
+chai.use(chaiAsPromised);
+chaiAsPromised.transferPromiseness = browser.transferPromiseness;
+
+describe('Impact', function() {
+
+    it('Login Page Title', function () {
+        openSearchUrl({});
+        // get title
+        var title = browser.getTitle();
+        assert.equal(title, 'IE ImpaCT Website');
+    });
+
+    it('Login into Impact', function () {
+    	browser.setValue('[name="userId"]','uatsuperuser01');
+    	browser.setValue('[name="password"]','Test1234%');
+    	browser.click('.LoginButton');
+    });
+
+    it('Dashboard', function () {
+    	// wait till page opens
+    	browser.waitForVisible('//div[@id="RightPannelHeading"]', 5000);
+
+    	it('Dashboard Title', function () {
+	    	// get title
+	        var title = browser.getTitle();
+	        assert.equal(title, 'ImpaCT - Welcome');
+        });
+
+    	it('Dashboard Element', function () {
+	    	var caseInfo = browser.getText('#RightPannelHeading*=CASE INFO');
+	    	assert.equal(caseInfo, 'CASE INFO');
+    	});
+
+    	it('Click on Others', function () {
+    	
+    	});
+    });
+               
+    openSearchUrl = function(opts) {
+    	//https://ussltcsnw2654.solutions.glbsnet.com:9443/wp/SELoginAccess.jsp?fromIndex=true
+    	//http://ussltcsnw2654.solutions.glbsnet.com:9088/wp/SELoginAccess.jsp
+        var url = "http://ussltcsnw2654.solutions.glbsnet.com:9088/wp/SELoginAccess.jsp?fromIndex=true";
+        console.log("Opening " + url);
+        return browser.url(url);
+    };
+    assert = require('assert');
+    sleep = function(ms) {
+        return function(done) {
+            setTimeout(done, ms);
+        }
+    };
+    getPath = function() {
+        var urlmodule = require("url");
+        return browser.url().then(function(res) {
+            return urlmodule.parse(res.value).path;
+        });
+    };
+    inspectElement = function(elt) {
+        var inspect = {};
+
+        // TODO: is there an easier way to do that?
+        return browser.elementIdName(elt.value.ELEMENT).then(function(res) {
+            inspect.tag = res.value;
+            return browser.elementIdText(elt.value.ELEMENT);
+        }).then(function(res) {
+            inspect.text = res.value;
+            return browser.elementIdLocation(elt.value.ELEMENT);
+        }).then(function(res) {
+            inspect.location = res.value;
+            console.log("Inspect: <" + inspect.tag + "> at ("+inspect.location.x+","+inspect.location.y+")");
+            return inspect;
+        })
+    };
+
+
+     printElement = function(ele) {
+        var selectedEle = {};
+        return browser.elementIdName(ele.value.ELEMENT).then(function(res) {
+            selectedEle.name = res.value;
+            return browser.elementIdAttribute(ele.value.ELEMENT,'id');
+        }).then(function(res) {
+            selectedEle.id = res.value;
+            var textId = '#'+selectedEle.id;
+            return browser.getValue(textId);
+        }).then(function(res) {
+            selectedEle.value = res;
+            console.log(selectedEle.name+" id:'"+selectedEle.id+"' value:'"+selectedEle.value+"'");
+            return selectedEle;
+        });
+    };
+});
