5305d0b4960fd880ab03d83b3cb8f2f6d5baf83c
JBaba <naimish101.viradia@gmail.com>
2016-12-01 16:57:15 -0500

    BI and ES Definations added

--- webdriverio-test/test/specs/Definations.js
+++ webdriverio-test/test/specs/Definations.js
@@ -1,1 +1,6 @@
-var es = { 1 : "#BITOP", 2 : "Benefit Issuance", 3 : "Essential Services", 4 : "#leftNavBIEPS"};
+module.exports = {
+	// Benefit Issuance Open Menu Definations
+	var bis = { 1 : "#BITOP", 2 : "Benefit Issuance", 3 : "Benefit Issuance Search", 4 : "h2*=Issuance Date Range"},
+	// Essential Service Open Menu Definations
+	var es = { 1 : "#BITOP", 2 : "Benefit Issuance", 3 : "Essential Services", 4 : "#leftNavBIEPS"},
+};
